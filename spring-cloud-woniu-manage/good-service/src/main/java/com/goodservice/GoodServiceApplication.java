af7c237b3fd3d91f5b717926e2531463c61ea094
邵鑫雨 <2424590457@qq.com>
2022-08-10 09:18:59 +0800

    创建项目

---
+++ spring-cloud-woniu-manage/good-service/src/main/java/com/goodservice/GoodServiceApplication.java
@@ -0,0 +1,13 @@
+package com.goodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodServiceApplication.class, args);
+    }
+
+}
