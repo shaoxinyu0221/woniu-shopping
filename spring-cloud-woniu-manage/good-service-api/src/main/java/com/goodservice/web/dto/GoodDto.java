eed816e9379ceb6870a013f2e91ae122b3b89b68
邵鑫雨 <2424590457@qq.com>
2022-08-10 20:27:41 +0800

    good-service-api

---
+++ spring-cloud-woniu-manage/good-service-api/src/main/java/com/goodservice/web/dto/GoodDto.java
@@ -0,0 +1,17 @@
+package com.goodservice.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 邵鑫雨
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/8/10 20:27
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GoodDto {
+}
