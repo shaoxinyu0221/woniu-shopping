2794400f00a716d5307a7ad088fb8d62762b7828
邵鑫雨 <2424590457@qq.com>
2022-08-10 10:14:01 +0800

    引入工具包

---
+++ spring-cloud-woniu-manage/commons-util/src/main/java/com/commons/utils/ResponseResult.java
@@ -0,0 +1,66 @@
+package com.commons.utils;
+
+
+import java.util.Objects;
+
+public class ResponseResult<T> {
+    private int code;
+    private String msg;
+    private T data;
+
+    public ResponseResult() {
+    }
+
+    public ResponseResult(int code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ResponseResult<?> that = (ResponseResult<?>) o;
+        return code == that.code && Objects.equals(msg, that.msg) && Objects.equals(data, that.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, msg, data);
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseResult{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
+
