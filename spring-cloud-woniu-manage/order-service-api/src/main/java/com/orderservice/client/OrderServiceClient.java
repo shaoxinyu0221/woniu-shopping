067588a27513e81321698b7a29316fea83665249
邵鑫雨 <2424590457@qq.com>
2022-08-10 20:24:29 +0800

    OrderServiceClient

--- spring-cloud-woniu-manage/order-service-api/src/main/java/com/orderservice/client/OrderServiceClient.java
+++ spring-cloud-woniu-manage/order-service-api/src/main/java/com/orderservice/client/OrderServiceClient.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.openfeign.FeignClient;
  * @description: TODO
  * @date 2022/8/10 17:56
  */
-@FeignClient(value = "/order",fallback = OrderServiceClientFallback.class)
+@FeignClient(value = "order-service",fallback = OrderServiceClientFallback.class)
 public interface OrderServiceClient {
 
 }
