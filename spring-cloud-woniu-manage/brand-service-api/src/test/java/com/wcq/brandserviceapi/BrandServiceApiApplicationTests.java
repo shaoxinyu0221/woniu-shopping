13cd11a96bd7a437578fa0dc66d3acb6df2e7cbf
wu <legend>
2022-08-11 10:29:05 +0800

    wuchaunqi[0811 10:28]创建项目模块

---
+++ spring-cloud-woniu-manage/brand-service-api/src/test/java/com/wcq/brandserviceapi/BrandServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcq.brandserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
