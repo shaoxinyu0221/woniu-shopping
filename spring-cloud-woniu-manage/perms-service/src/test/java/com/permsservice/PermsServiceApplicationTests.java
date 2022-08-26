7a6dc059c3fc0a0be41ea486c924cf7d29fc2ca7
邵鑫雨 <2424590457@qq.com>
2022-08-26 18:01:46 +0800

    保存

--- spring-cloud-woniu-manage/perms-service/src/test/java/com/permsservice/PermsServiceApplicationTests.java
+++ spring-cloud-woniu-manage/perms-service/src/test/java/com/permsservice/PermsServiceApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.*;
 
 @SpringBootTest
 @Transactional
@@ -22,6 +23,33 @@ class PermsServiceApplicationTests {
         for (RbacPermsPo perm : parentPerms) {
             System.out.println(perm.getPercode());
         }
+    }
+
+
+    @Test
+    public void demo3() throws ExecutionException, InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(3);
+
+        Callable<String> callable = () -> {
+            System.out.println("想想想想想"); System.out.flush();
+            Thread.sleep(3000);
+            return "hello world";
+        };
+        System.out.println("问问题");
+        Future<String> future = service.submit(callable);
+        // Mono<String> "装"的 callable 的执行结果。
+
+        System.out.print("做其他题目");
+        for (int i = 0; i <100; i++) {
+            System.out.flush();
+            System.out.print(".");
+            System.out.flush();
+            Thread.sleep(200);
+        }
+        System.out.println("");
+//        System.out.println(future.get());
+
+        System.out.println("拿到问题的结果：" + future.get());
     }
 
 }
